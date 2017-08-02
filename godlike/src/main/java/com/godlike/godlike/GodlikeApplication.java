4573998c555424a59040d2e52646d330cbbd11de
dingmengjiao <570657241@qq.com>
2017-08-02 16:02:16 +0800

    添加godlike项目

---
+++ godlike/src/main/java/com/godlike/godlike/GodlikeApplication.java
@@ -0,0 +1,12 @@
+package com.godlike.godlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodlikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodlikeApplication.class, args);
+	}
+}
