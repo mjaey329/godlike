e7f235e93f2f2c0ac016da0cfb0d0d1566176cea
dingmengjiao <570657241@qq.com>
2017-08-03 15:01:16 +0800

    1

--- godlike/src/main/java/com/godlike/godlike/GodlikeApplication.java
+++ godlike/src/main/java/com/godlike/godlike/GodlikeApplication.java
@@ -2,11 +2,18 @@ package com.godlike.godlike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GodlikeApplication {
-
+    @RequestMapping("/hell")
+    public String index() {
+        return "Hello World xx";
+    }
 	public static void main(String[] args) {
+		System.out.println("123");
 		SpringApplication.run(GodlikeApplication.class, args);
 		
 		
